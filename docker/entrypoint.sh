348f52dbff846697d2c73afd1717ca69fa73c399
axute <github@axute.tech>
2021-05-15 18:11:13 +0200

    feat: add debug option

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,9 +3,15 @@
 /generate_lftp_exludes.sh
 
 export LFTP_PASSWORD="${PLUGIN_PASSWORD}"
-LFTP_COMMAND=""
+if [ -n "${PLUGIN_DEBUG}" ]
+then
+  LFTP_COMMAND="debug;"
+else
+  LFTP_COMMAND=""
+fi
+
 LFTP_COMMAND+="mkdir -p -f ${PLUGIN_TARGET};"
-LFTP_COMMAND+="mirror -eRvvv --exclude-rx-from=/lftp_excludes.rx --delete-excluded ${PLUGIN_SOURCE} ${PLUGIN_TARGET};"
+LFTP_COMMAND+="mirror -eRvvv --scan-all-first --exclude-rx-from=/lftp_excludes.rx --delete-excluded ${PLUGIN_SOURCE} ${PLUGIN_TARGET};"
 LFTP_COMMAND+="bye"
 echo "start transfer from '${PLUGIN_SOURCE}' to '${PLUGIN_HOST}:${PLUGIN_TARGET}' ..." | ts
 lftp --env-password "sftp://${PLUGIN_USERNAME}@${PLUGIN_HOST}"  -e "${LFTP_COMMAND}" | ts
